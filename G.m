9851e2c5d21dcbb74ddfa5547e601d59b09755e4
Alejandro Romero Rivas <103322691+FR4NC1SK0@users.noreply.github.com>
2025-09-25 16:42:48 +0200

    Delete G.m

--- G.m
+++
@@ -1,9 +0,0 @@
-function out = G(omegabar,sigma)
-mu1 =log(1/(sigma^2+1)^(1/2));   %mean of the logarithm of a lognormal variable whose mean is 1
-sigma1 = log(sigma^2+1)^(1/2);      %standard deviation of the logarithm of a lognormal variable whose mean is 1        
-
-% Define the PDF function
-pdf_func = @(x) lognpdf(x,mu1,sigma1);
-% Compute the truncated expectation
-out = integral(@(x) x .* pdf_func(x), 0, omegabar);
-end
